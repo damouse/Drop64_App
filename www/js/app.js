acc69e0781b61cd66637d0cd2bdb53c05a4729d3
damouse <mickey.barboi@gmail.com>
2017-02-14 16:12:24 -0600

    comm channel

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,26 @@
-// var io = require('socket.io')
-
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    
-    if(window.StatusBar) {
+
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
 
 .controller('RootController', function($scope) {
-  $scope.hello = "Hello";
+  var socket = io.connect('localhost:3000');
+
+  socket.on('connect', function() {
+    console.log("Connected");
+    setInterval(function() {
+      socket.emit("controller", "Controller");
+    }, 1000);
+  });
+
 })
